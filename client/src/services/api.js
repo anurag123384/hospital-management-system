d96e36b595995aa7b5a6167f4c0707b0c174248d
Anurag Kumar Singh <yourgmail@gmail.com>
2026-08-01 14:07:14 +0530

    Update backend API URL for deployment

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
 
-  baseURL:"http://localhost:5000/api",
+ baseURL: "https://hospital-management-system-k3n2.onrender.com/api",
 
   headers:{
     "Content-Type":"application/json",
